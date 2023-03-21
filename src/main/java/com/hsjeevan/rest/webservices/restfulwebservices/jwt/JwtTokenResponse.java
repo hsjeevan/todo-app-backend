e45555422b981bf967aa9761dcc35c925e0e8179
Jeevan <hsjeevan07@gmail.com>
2023-03-20 22:35:20 -0400

    setup mysql on docker

---
+++ src/main/java/com/hsjeevan/rest/webservices/restfulwebservices/jwt/JwtTokenResponse.java
@@ -0,0 +1,5 @@
+package com.hsjeevan.rest.webservices.restfulwebservices.jwt;
+
+public record JwtTokenResponse(String token) {}
+
+
---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into TODO (ID ,DESCRIPTION ,DONE ,TARGET_DATE ,USERNAME )
+values (1001,'Learn AWS',false,Current_Date(),'hsjeevan');
+
+insert into TODO (ID ,DESCRIPTION ,DONE ,TARGET_DATE ,USERNAME )
+values (1002,'Learn GCP',false,Current_Date(),'hsjeevan');
+
+insert into TODO (ID ,DESCRIPTION ,DONE ,TARGET_DATE ,USERNAME )
+values (1003,'Learn Devops',false,Current_Date(),'hsjeevan');
